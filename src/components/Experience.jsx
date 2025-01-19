7678bc26258530d953647486f42a98bcdfe89546
talhabilal-dev <m.talha.bilal.dev@gmail.com>
2025-01-19 23:57:31 +0500

    Large device responsivness issue

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -12,7 +12,7 @@ export default function Experience() {
             My journey into development began, laying the foundation with
             essential skills and small projects.
           </p>
-          <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+          <div className="grid sm:grid-cols-1 md:grid-cols-2  gap-4">
             <div className="rounded-lg min-h-[200px] w-full shadow-md items-center flex justify-around flex-col text-black dark:text-slate-100 bg-[#240046] p-4">
               <div>
                 <Image src="/images/exp1.svg" width={80} height={80} alt="image" />
@@ -69,7 +69,67 @@ export default function Experience() {
           <p className="text-neutral-800 dark:text-neutral-200 text-sm md:text-xl font-bold mb-8">
             Laid the groundwork for my professional career with diverse projects.
           </p>
-          <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+          <div className="grid sm:grid-cols-1 md:grid-cols-2  gap-4">
+            <div className="rounded-lg min-h-[200px] w-full shadow-md items-center flex justify-around flex-col text-black dark:text-slate-100 bg-[#240046] p-4">
+              <div>
+                <Image src="/images/exp1.svg" width={80} height={80} alt="image" />
+              </div>
+              <h3 className="Heading font-bold text-base md:text-lg">
+                Management System in Java
+              </h3>
+              <p className="text-xs md:text-sm p-2">
+                Built a basic management system to strengthen my object-oriented
+                programming skills.
+              </p>
+            </div>
+            <div className="rounded-lg min-h-[200px] w-full shadow-md items-center flex justify-around flex-col text-black dark:text-slate-100 bg-[#240046] p-4">
+              <div>
+                <Image src="/images/exp1.svg" width={80} height={80} alt="image" />
+              </div>
+              <h3 className="Heading font-bold text-base md:text-lg">
+                Node.js and SQL
+              </h3>
+              <p className="text-xs md:text-sm p-2">
+                Learned backend development with Node.js and SQL for scalable
+                applications.
+              </p>
+            </div>
+            <div className="rounded-lg min-h-[200px] w-full shadow-md items-center flex justify-around flex-col text-black dark:text-slate-100 bg-[#240046] p-4">
+              <div>
+                <Image src="/images/exp1.svg" width={80} height={80} alt="image" />
+              </div>
+              <h3 className="Heading font-bold text-base md:text-lg">
+                Backend Developer Role
+              </h3>
+              <p className="text-xs md:text-sm p-2">
+                Worked as a backend developer at a tech startup, gaining
+                real-world experience.
+              </p>
+            </div>
+            <div className="rounded-lg min-h-[200px] w-full shadow-md items-center flex justify-around flex-col text-black dark:text-slate-100 bg-[#240046] p-4">
+              <div>
+                <Image src="/images/exp1.svg" width={80} height={80} alt="image" />
+              </div>
+              <h3 className="Heading font-bold text-base md:text-lg">
+                Laboratory Management System
+              </h3>
+              <p className="text-xs md:text-sm p-2">
+                Developed a comprehensive system in Node.js for efficient
+                laboratory workflows.
+              </p>
+            </div>
+          </div>
+        </div>
+      ),
+    },
+    {
+      title: "Building Career Skills (Early 2024)",
+      content: (
+        <div>
+          <p className="text-neutral-800 dark:text-neutral-200 text-sm md:text-xl font-bold mb-8">
+            Laid the groundwork for my professional career with diverse projects.
+          </p>
+          <div className="grid sm:grid-cols-1 md:grid-cols-2  gap-4">
             <div className="rounded-lg min-h-[200px] w-full shadow-md items-center flex justify-around flex-col text-black dark:text-slate-100 bg-[#240046] p-4">
               <div>
                 <Image src="/images/exp1.svg" width={80} height={80} alt="image" />
